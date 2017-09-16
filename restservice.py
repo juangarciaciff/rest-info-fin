751f21fa8d5e36df6647677e021e5d292247974c
Juan García <juangarcia@campusciff.net>
2017-09-16 07:48:33 +0200

    Create restservice.py

---
+++ restservice.py
@@ -0,0 +1,12 @@
+from sys import argv
+import bottle
+from bottle import route,run
+
+bottle.debug(True)
+
+@route('/')
+def index():
+	return "<h1>Hola Mundo2!!!</h1>"
+
+if __name__ == '__main__':
+run(host='0.0.0.0',port=argv[1])
